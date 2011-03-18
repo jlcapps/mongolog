f9c211462717d72e7b19a8ff973b7af68b010c97
Lee Capps <himself@leecapps.com>
2011-03-18 08:53:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongolog::Application.config.secret_token = '7eea69586e86b123a5caec6d3b2fa4912314b57572a3c3e2eb3be8aeb1d95a023ad401f364c4c7a1301461d671ca22a334b9fc2cfbf20521f57599636d9933f2'
